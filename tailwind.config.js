69df44f4d7dd1726dbf4d044c1a527d2e93eda0e
rerro <101159509+rorre0@users.noreply.github.com>
2023-07-28 13:45:28 +0900

    feat: add common component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./client/**/*.html"],
+  content: ['./client/**/*.html'],
   theme: {
     extend: {},
+  },
+  colors: {
+    primary: '#5f0080',
+    secondary: '#bd76ff',
+    content: '#333333',
+    error: '#f03f40',
+    accent: '#A80000',
+  },
+  fontFamily: {
+    sans: ['Noto Sans KR', 'sans-serif'],
+  },
+  fontSize: {
+    xs: '0.625rem',
+    sm: '0.75rem',
+    base: '1rem',
+    lg: '1.375rem',
+    xl: '2.375rem',
   },
   plugins: [],
 };
