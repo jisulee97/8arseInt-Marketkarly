9afc352d03ff6f006a4aca2811a2fa129bf4f115
rerro <101159509+rorre0@users.noreply.github.com>
2023-07-28 10:33:06 +0900

    feat: - .eslintrc.js - prettier.config.js - image

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'eslint-config-prettier',
+    // "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'off',
+  },
+};
