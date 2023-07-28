282257d6575bb8c517e4b9e4e922b5ea6e98ea80
rerro <101159509+rorre0@users.noreply.github.com>
2023-07-28 15:50:11 +0900

    style: tailwind.config.js / css 경로 수정, link 경로 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,25 @@
 module.exports = {
   content: ['./client/**/*.html'],
   theme: {
-    extend: {},
-  },
-  colors: {
-    primary: '#5f0080',
-    secondary: '#bd76ff',
-    content: '#333333',
-    error: '#f03f40',
-    accent: '#A80000',
-  },
-  fontSize: {
-    xs: '0.625rem',
-    sm: '0.75rem',
-    base: '1rem',
-    lg: '1.375rem',
-    xl: '2.375rem',
+    extend: {
+      colors: {
+        primary: '#5f0080',
+        secondary: '#bd76ff',
+        content: '#333333',
+        error: '#f03f40',
+        accent: '#A80000',
+      },
+      fontSize: {
+        xs: '0.625rem',
+        sm: '0.75rem',
+        base: '1rem',
+        lg: '1.375rem',
+        xl: '2.375rem',
+      },
+      maxWidth: {
+        mw: '1920px',
+      },
+    },
   },
   plugins: [],
 };
