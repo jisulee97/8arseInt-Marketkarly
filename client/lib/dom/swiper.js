b3f9e7379c01d015b0da1bce9122335ae8bb4167
leejisu <ejeesoo97@gmail.com>
2023-07-31 02:57:10 +0900

    Feat : swiper.js
    - main banner, 이 상품 어때요 스와이퍼 코드 구현
    
    #21 진행중

--- client/lib/dom/swiper.js
+++ client/lib/dom/swiper.js
@@ -3,10 +3,11 @@ import { tiger } from './../utils/tiger.js';
 import { getNode } from './getNode.js';
 import { addClass } from './css.js';
 
-async function mainBanner() {
+async function Main() {
   const { data } = await tiger.get('http://localhost:3000/main');
+
   data.banner.forEach((item) => {
-    const swipperWrapper = getNode('.swiper-wrapper');
+    const swipperWrapper = getNode('.banner__slider > .swiper-wrapper');
 
     const swipperItem = document.createElement('div');
     addClass(swipperItem, 'swiper-slide');
@@ -22,6 +23,24 @@ async function mainBanner() {
     swipperWrapper.append(swipperItem);
   });
 
+  data.products.forEach((item) => {
+    const swipperWrapper = getNode('.recommend__slider > .swiper-wrapper');
+
+    const swipperItem = document.createElement('div');
+    addClass(swipperItem, 'swiper-slide');
+
+    const itemImage = document.createElement('img');
+    itemImage.src = item.image.thumbnail;
+    itemImage.alt = item.image.alt;
+    itemImage.style.objectFit = 'cover';
+    itemImage.style.width = '249px';
+    itemImage.style.height = '320px';
+
+    swipperItem.append(itemImage);
+
+    swipperWrapper.append(swipperItem);
+  });
+
   const swiper = new Swiper('.banner__slider', {
     speed: 400,
     spaceBetween: 100,
@@ -31,5 +50,17 @@ async function mainBanner() {
       prevEl: '.swiper-button-prev',
     },
   });
+
+  const recommendSwiper = new Swiper('.recommend__slider', {
+    speed: 400,
+    spaceBetween: 100,
+    loop: true,
+    slidesPerView: 4,
+    slidesPerGroupSkip: 4,
+    navigation: {
+      prevEl: '.recommend__slider > .swiper-button-prev',
+      nextEl: '.recommend__slider > .swiper-button-next',
+    },
+  });
 }
-mainBanner();
+Main();
