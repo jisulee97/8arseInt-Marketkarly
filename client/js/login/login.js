fca980029b88c05df30b0604ba2d4a076ef38960
LKJ970524 <dlrbwjd7096@naver.com>
2023-07-30 23:06:38 +0900

    Feat
    login.js 기본 설정 및 변수 지정
    
    #23

---
+++ client/js/login/login.js
@@ -0,0 +1,27 @@
+
+const user = {
+  id:'asd@karly.com',
+  pw:'karly123!@'
+}
+
+
+
+function emailReg(text) {
+  // eslint-disable-next-line
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(text).toLowerCase());
+}
+
+function pwReg(text) {
+  const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
+  return re.test(String(text).toLowerCase());
+}
+
+
+export const idInput = document.getElementById('userEmail')
+export const pwInput = document.getElementById('userPassword')
+export const loginB = document.querySelector('.btn-login')
+
+
+
+
