28012d11088fe992aec5813305f6eb5ebbd337c9
rerro <101159509+rorre0@users.noreply.github.com>
2023-07-30 12:30:27 +0900

    Style: cart 스타일 완료

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,14 +11,14 @@ module.exports = {
         accent: '#A80000',
       },
       fontSize: {
-        xs: '0.625rem',
-        sm: '0.75rem',
-        base: '1rem',
-        lg: '1.375rem',
-        xl: '2.375rem',
+        xs: '0.625rem' /* 10px */,
+        sm: '0.75rem' /* 12px */,
+        base: '1rem' /* 16px */,
+        lg: '1.375rem' /* 22px */,
+        xl: '2.375rem' /* 38px */,
       },
-      maxWidth: {
-        mw: '1920px',
+      width: {
+        container: '1050px',
       },
     },
   },
