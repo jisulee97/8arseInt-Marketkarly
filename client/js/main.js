3ce4a96639dbcf692b4366cda077e93fc9e4f4c9
LKJ970524 <dlrbwjd7096@naver.com>
2023-07-31 23:44:53 +0900

    Feat
    - mainpage-popup.js  함수 추가
    
    #33

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,5 @@
 import { getNode } from './../lib/dom/getNode.js';
-import { fadeout } from "./index.js";
+// import { fadeout, } from "./index.js";
 
 
 
@@ -31,6 +31,14 @@ function removePromotion() {
 removePromotion();
 
 
+// 팝업창 제거 함수
+export function fadeout() {
+  const close = document.querySelector('.popupButton')
+  const popup = document.querySelector('.popupWrapper')
+  close.addEventListener('click', () => {
+    popup.classList.add('hidden')
+  })
+}
 fadeout()
 
 
