d0347ee4c101d7e031e474ba8032e84a93333182
rerro <101159509+rorre0@users.noreply.github.com>
2023-08-01 02:16:59 +0900

    Modify: js 분리 및 수정

--- client/js/main.js
+++ client/js/main.js
@@ -3,34 +3,6 @@ import { getNode } from './../lib/dom/getNode.js';
 
 
 
-
-
-
-// 카테고리 리스트 토글 함수
-
-function categoryToggle() {
-  const categoryMenuBtn = getNode('.menu__button__category');
-  const dropDownMenu = getNode('.category');
-
-  categoryMenuBtn.addEventListener('click', () => {
-    dropDownMenu.classList.toggle('hidden');
-  });
-}
-categoryToggle();
-
-// 프로모션 제거 함수
-
-function removePromotion() {
-  const promotionBtn = getNode('.promotion__button');
-  const promotion = getNode('.promotion');
-
-  promotionBtn.addEventListener('click', () => {
-    promotion.classList.toggle('hidden');
-  });
-}
-removePromotion();
-
-
 // 팝업창 제거 함수
 export function fadeout() {
   const close = document.querySelector('.popupButton')
@@ -40,5 +12,4 @@ export function fadeout() {
   })
 }
 fadeout()
-
 
