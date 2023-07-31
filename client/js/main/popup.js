62faf4f631934b74158e903d982b42b4ed9fe273
LKJ970524 <dlrbwjd7096@naver.com>
2023-07-31 23:40:20 +0900

    Remove
    - popup.js  삭제
    
    #33

--- client/js/main/popup.js
+++
@@ -1,9 +0,0 @@
-const close = document.querySelector('.popupButton')
-const popup = document.querySelector('.popupWrapper')
-
-export function fadeout() {
-  close.addEventListener('click', () => {
-    popup.classList.add('hidden')
-  })
-}
-
